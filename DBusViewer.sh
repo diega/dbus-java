7deabc36b42e99b65ee18548fbbf923601c5d2f5
Matthew Johnson <mjj29@hecate.trinhall.cam.ac.uk>
2007-01-13 22:12:37 +0000

    argh!!

--- DBusViewer.sh
+++ DBusViewer.sh
@@ -7,4 +7,4 @@ JARPATH=%JARPATH%
 JAVAUNIXLIBPATH=%JAVAUNIXLIBPATH%
 JAVAUNIXJARPATH=%JAVAUNIXJARPATH%
 
-exec $JAVA -DPid=$$ -DVersion=$VERSION -Djava.library.path=$JAVAUNIXLIBPATH -cp $JAVAUNIXJARPATH/unix.jar:$JAVAUNIXJARPATH/debug-$DEBUG.jar:$JAVAUNIXJARPATH/hexdump.jar:$JARPATH/dbus.jar:$JARPATH/dbus-bin.jar:$JARPATH/dbus-viewer.jar org.freedesktop.dbus.bin.viewer.DBusViewer "$@"
+exec $JAVA -DPid=$$ -DVersion=$VERSION -Djava.library.path=$JAVAUNIXLIBPATH -cp $JAVAUNIXJARPATH/unix.jar:$JAVAUNIXJARPATH/debug-$DEBUG.jar:$JAVAUNIXJARPATH/hexdump.jar:$JARPATH/dbus.jar:$JARPATH/dbus-bin.jar:$JARPATH/dbus-viewer.jar org.freedesktop.dbus.viewer.DBusViewer "$@"
