43da57ca0e196b32a06b139af8c45d1fb0c11efd
Matthew Johnson <mjj29@adonis.trinhall.cam.ac.uk>
2008-02-03 11:26:13 +0000

    add profile

--- org/freedesktop/dbus/test/ProfilerInstance.java
+++ org/freedesktop/dbus/test/ProfilerInstance.java
@@ -17,6 +17,7 @@ public class ProfilerInstance implements Profiler
 {
    public boolean isRemote() { return false; }
    public void array(int[] v) { return; }
+   public void stringarray(String[] v) { return; }
    public void map(Map<String,String> m) { return; }
    public void list(List<String> l) { return; }
    public void bytes(byte[] b) { return; }
