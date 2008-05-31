466b738fa9bd7d1c0165ad69bf4d09c4a48a60c8
Matthew Johnson <mjj29@hecate.matthew.ath.cx>
2008-05-31 20:39:52 +0100

    relicence to LGPL

--- org/freedesktop/dbus/bin/ListDBus.java
+++ org/freedesktop/dbus/bin/ListDBus.java
@@ -3,7 +3,7 @@
    Copyright (c) 2005-2006 Matthew Johnson
 
    This program is free software; you can redistribute it and/or modify it
-   under the terms of either the GNU General Public License Version 2 or the
+   under the terms of either the GNU Lesser General Public License Version 2 or the
    Academic Free Licence Version 2.1.
 
    Full licence texts are included in the COPYING file with this program.
--- org/freedesktop/dbus/viewer/SaveAllAction.java
+++ org/freedesktop/dbus/viewer/SaveAllAction.java
@@ -3,7 +3,7 @@
    Copyright (c) 2006 Peter Cox
 
    This program is free software; you can redistribute it and/or modify it
-   under the terms of either the GNU General Public License Version 2 or the
+   under the terms of either the GNU Lesser General Public License Version 2 or the
    Academic Free Licence Version 2.1.
 
    Full licence texts are included in the COPYING file with this program.
